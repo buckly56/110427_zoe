165fdfa77b007ce05feb87f5d43bae75fd4c1145
buckly56 <45410163+buckly56@users.noreply.github.com>
2021-04-27 19:29:14 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
+let A = 0
+input.onButtonPressed(Button.A, function () {
+    A = 2
+    basic.showNumber(A)
+    basic.pause(100)
+    for (let index = 0; index < 4; index++) {
+        A += 1
+        basic.showNumber(A)
+    }
 })
